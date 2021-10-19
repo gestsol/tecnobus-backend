0ec116c6777a602644728757e729658e3361aa05
Leonardo Ramos <leorrc96@gmail.com>
2021-10-19 18:18:36 -0400

    update(config.ex): set tesla for api calls and Scheduler to process alerts with AlertListener module

--- config/config.exs
+++ config/config.exs
@@ -26,6 +26,18 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# Use Hackney for api calls with tesla
+config :tesla, adapter: Tesla.Adapter.Hackney, recv_timeout: 120_000
+
+
+config :tecnobus, Tecnobus.Scheduler,
+  jobs: [
+    process_alerts: [
+      schedule: "/10 * * * *",
+      task: {Tecnobus.AlertListener, :process_centinela_alerts, [10]}
+    ]
+  ]
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
