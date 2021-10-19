ed8d19d1c6db96fc86a4219495b866a57ad2111f
Leonardo Ramos <leorrc96@gmail.com>
2021-10-19 18:14:29 -0400

    feat(API/alerts.ex): create module to make api calls to tecnobus alerts

---
+++ lib/tecnobus/API/alerts.ex
@@ -0,0 +1,55 @@
+defmodule Tecnobus.API.Alerts do
+  alias Tecnobus.API.BodyHelper.AlertRequest
+  use Tesla, only: [:post, :get]
+
+  plug Tesla.Middleware.Timeout, timeout: 120_000
+  plug Tesla.Middleware.BaseUrl, "http://181.212.4.22:12056/api/v1/basic"
+  plug Tesla.Middleware.Headers, [{"Content-Type", "application/json"}]
+  plug Tesla.Middleware.JSON
+
+  def get_auth_key() do
+    {:ok, response} = get("/key?username=wit&password=wit3004..")
+
+    Map.get(response, :body)
+    |> Map.get("data")
+    |> Map.get("key")
+  end
+
+  def get_groups do
+    do_request("get", "/groups")
+  end
+
+  def get_devices do
+    do_request("get", "/devices")
+  end
+
+  def get_devices_by_group(group_id) do
+    get_devices()
+    |> Enum.filter(fn d -> d["groupid"] === group_id end)
+  end
+
+  def get_alerts(body = %AlertRequest{}) do
+    do_request("post", "/alarm/detail", Map.from_struct(body))
+  end
+
+  def do_request("post", endpoint, body) do
+    auth_key = get_auth_key()
+
+    post(endpoint, Map.put(body, "key", auth_key))
+    |> extract_response_data()
+  end
+
+  def do_request("get", endpoint) do
+    auth_key = get_auth_key()
+
+    get("#{endpoint}?key=#{auth_key}")
+    |> extract_response_data()
+  end
+
+  def extract_response_data(api_call) do
+    {:ok, response} = api_call
+
+    Map.get(response, :body)
+    |> Map.get("data")
+  end
+end
