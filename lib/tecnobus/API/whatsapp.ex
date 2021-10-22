1aed26285f72caff9526b24ed76a5f719f468942
Leonardo Ramos <leorrc96@gmail.com>
2021-10-22 11:15:13 -0400

    update(whatsapp.ex): se hace uso de api de wassi

--- lib/tecnobus/API/whatsapp.ex
+++ lib/tecnobus/API/whatsapp.ex
@@ -2,18 +2,14 @@ defmodule Tecnobus.API.Whatsapp do
 
   use Tesla, only: [:post, :get]
 
-  plug Tesla.Middleware.Timeout, timeout: 120_000
-  plug Tesla.Middleware.BaseUrl, "https://message-backend.gestsol.io"
-  plug Tesla.Middleware.Headers, [{"Content-Type", "application/json"}]
+  plug Tesla.Middleware.BaseUrl, "https://api.wassi.chat/v1"
+  plug Tesla.Middleware.Headers, [{"token", "f72b338f2ee531332e95824c0c8e74c6a0b60da4b2f521f82c3fb42b6815a444d33d4ec2a52c056f"}]
+  plug Tesla.Middleware.Logger
   plug Tesla.Middleware.JSON
 
-  def send(code, phone, message) do
-    {:ok, response} = post(
-      "/whatsapp/sendmessage",
-      %{ code: code, phone: phone, message: message },
-      opts: [adapter: [recv_timeout: 120_000]]
-    )
+  @device "61683609a25fda1c540fa654"
 
-    Map.get(response, :body) |> IO.inspect()
+  def send(to, message) when is_bitstring(to) do
+    post("/messages", %{"phone" => to, "message" => message, "device" => @device})
   end
 end
